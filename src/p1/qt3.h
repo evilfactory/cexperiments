86c8f552cb1fec5a3d3029b07e37f564b2b1a552
Evil Factory <36804725+evilfactory@users.noreply.github.com>
2025-05-12 11:58:00 -0300

    Update qt3.h

--- src/p1/qt3.h
+++ src/p1/qt3.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int value;
+    int input;
     printf("Digite o numero: ");
-    scanf("%d", &value);
+    scanf("%d", &input);
 
-    int originalValue = value;
+    int value = input;
+
     int remainder;
 
     int n100, n50, n20, n10, n5, n2;
@@ -36,35 +37,67 @@ int main() {
     n2 = remainder / 2;
     remainder %= 2;
 
-    if (remainder == 1 && n2 >= 2) {
-        n2 -= 2;
-        n5 += 1;
-        remainder = 0;
-    }
-    else if (remainder == 1 && n5 >= 1)
+    if (remainder == 1)
     {
-        n2 += 3;
-        n5 -= 1;
-        remainder = 0;
+        if (n5 >= 1)
+        {
+            n5 -= 1;
+            n2 += 3;
+            remainder = 0;
+        }
+        else if (n10 >= 1)
+        {
+            n10 -= 1;
+            n5 += 1;
+            n2 += 3;
+            remainder = 0;
+        }
+        else if (n20 >= 1)
+        {
+            n20 -= 1;
+            n10 += 1;
+            n5 += 1;
+            n2 += 3;
+            remainder = 0;
+        }
+        else if (n50 >= 1)
+        {
+            n50 -= 1;
+            n20 += 2;
+            n10 += 0;
+            n5 += 1;
+            n2 += 3;
+            remainder = 0;
+        }
+        else if (n100 >= 1)
+        {
+            n100 -= 1;
+            n50 += 1;
+            n20 += 1;
+            n10 += 2;
+            n5 += 1;
+            n2 += 3;
+            remainder = 0;
+        }
     }
 
-    printf("\nNotas necessarias para %d:\n", originalValue);
+    if (remainder != 0)
+    {
+        printf("Sobrou R$%d que nao pode ser representado com as notas disponiveis\n", remainder);
+    }
+    if (n100 * 100 + n50 * 50 + n20 * 20 + n10 * 10 + n5 * 5 + n2 * 2 != value)
+    {
+        printf("Error in %d. %d != %d\n", value, n100 * 100 + n50 * 50 + n20 * 20 + n10 * 10 + n5 * 5 + n2 * 2, value);
+    }
+
+
+    printf("\nNotas necessarias para %d:\n", value);
     printf("R$100: %d\n", n100);
     printf("R$50 : %d\n", n50);
     printf("R$20 : %d\n", n20);
     printf("R$10 : %d\n", n10);
     printf("R$5  : %d\n", n5);
     printf("R$2  : %d\n", n2);
-
-    if (remainder != 0)
-    {
-        printf("Sobrou R$%d que nao pode ser representado com as notas disponiveis\n", remainder);
-    }
-
-    if (n100 * 100 + n50 * 50 + n20 * 20 + n10 * 10 + n5 * 5 + n2 * 2 != originalValue)
-    {
-        printf("Error in %d.\n", originalValue);
-    }
 
     return 0;
 }
